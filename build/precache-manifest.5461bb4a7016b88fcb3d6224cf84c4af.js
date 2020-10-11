ad9abb2a7d0864aa01ecd879a38332e4ab081eee
devuzair56 <72253079+devuzair56@users.noreply.github.com>
2020-10-11 11:37:30 +0500

    Add files via upload

---
+++ build/precache-manifest.5461bb4a7016b88fcb3d6224cf84c4af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb61ded9717090b5c8cd19d0e91a55b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dd8f5243243fbb3c11c",
+    "url": "/static/css/main.78c7c615.chunk.css"
+  },
+  {
+    "revision": "ef7708b0d3bc1d9a3e79",
+    "url": "/static/js/2.d6d7f26b.chunk.js"
+  },
+  {
+    "revision": "890fe60e51f18d9e8e0004117fd20dc3",
+    "url": "/static/js/2.d6d7f26b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dd8f5243243fbb3c11c",
+    "url": "/static/js/main.69d6aab3.chunk.js"
+  },
+  {
+    "revision": "241f87120d285ee26494",
+    "url": "/static/js/runtime-main.56ae10a7.js"
+  }
+]);
